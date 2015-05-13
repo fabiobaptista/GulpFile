14d6a405b2523467d9c024ff9acfa3ef48e9de8b
FabioBaptista <fhbaptista@gmail.com>
2015-05-12 21:49:12 -0300

    less and sass task adjusts, project structure updates

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,4 +1,5 @@
 
+//Includes
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
@@ -7,46 +8,65 @@ var connect = require('gulp-connect');
 var watch = require('gulp-watch');
 
 var less = require('gulp-less');
-var uglify = require('gulp-uglify');
-var uglifycss = require('gulp-uglifycss');
+var sass = require('gulp-sass');
 
-var htmlPath = "./*.html";
-var cssPath = "./css";
-var lessFiles = "./less/**/*.less";
+//var uglify = require('gulp-uglify');
+//var uglifycss = require('gulp-uglifycss');
+
+//Variables
+var clientPath = "./src/client/";
+
+var tempPath = './.tmp/';
+
+var lessFiles = clientPath + "./styles/**/*.less";
+var sassFiles = clientPath + "./styles/**/*.scss";
+
+var htmlFiles = clientPath + "**/*.html";
+var cssFiles = tempPath +  "**/*.css";
+var jsFiles = clientPath + "**/*.js";
+
+//Pre processors
+gulp.task("less",function(){
+	gulp.src(lessFiles)
+		.pipe(concat(lessFiles))
+		.pipe(rename('main.less.css'))
+		.pipe(less())
+		//.pipe(uglifycss())
+		.pipe(gulp.dest(tempPath));
+});
+ 
+gulp.task("sass", function () {
+    gulp.src(sassFiles)
+		.pipe(concat(sassFiles))
+		.pipe(rename('main.sass.css'))
+		.pipe(sass().on('error', sass.logError))
+		//.pipe(uglifycss())
+        .pipe(gulp.dest(tempPath));
+});
+
+gulp.task("pre-processors",["less",	"sass"]);
+
+gulp.task("default",["less", "sass", "server", "livereload", "watch"]);
+
+//Server tasks
+gulp.task('watch-pre-processors', function() {
+    gulp.watch([lessFiles, sassFiles] , ["less", "sass"]);
+});
 
 gulp.task("server", function(){
 	connect.server({
+		port: 80,
 		livereload: true,
-		root: ['.']
+		root: [__dirname, clientPath]
 	});
 });
 
 gulp.task("livereload",function(){
 
-	var files = [htmlPath, cssPath];
+	var files = [htmlFiles, cssFiles, jsFiles];
 	gulp.src(files)
 		.pipe(watch(files))
     	.pipe(connect.reload());
-
 });
 
-gulp.task("less",function(){
-	gulp.src(lessFiles)
-		.pipe(concat(cssPath))
-		.pipe(rename('main.css'))
-		.pipe(less())
-		.pipe(uglifycss())
-		.pipe(gulp.dest(cssPath));
-		//.pipe(connect.reload());
-});
-
-gulp.task('watch', function() {
-    gulp.watch(lessFiles , ["less"]);
-});
-
-gulp.task("default",[
-	"less", 
-	"server", 
-	"livereload",
-	"watch"
-]);
+gulp.task("runserve",["server",	"livereload", "watch-pre-processors"]);
